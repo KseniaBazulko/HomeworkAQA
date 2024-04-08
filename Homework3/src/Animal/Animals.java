698966aafe488e094cf6e9eee1908c96791e546d
KseniaBazulko <bazulko001@mail.ru>
2024-04-08 23:01:22 +0300

    Add files via upload

---
+++ Homework3/src/Animal/Animals.java
@@ -0,0 +1,19 @@
+package Animal;
+
+public abstract class Animals {
+    private int runMaxDistance;
+     private int swimMaxDistance;
+    public static int getCount() {
+        return count;    }
+    private static int count;
+    public Animals(int runMaxDistance, int swimMaxDistance) {
+        this.runMaxDistance = runMaxDistance;
+        this.swimMaxDistance = swimMaxDistance;
+        count++;
+
+    }    public void run(int runDistance) {
+        System.out.println(runDistance <= runMaxDistance ? " пробежал " + runDistance + "м" : " не может бежать");    }
+    public void swim(int swimDistance) {
+         System.out.println(swimDistance < swimMaxDistance ? " проплыл " + swimDistance + "м" : " не может плыть");
+    }
+}
---
+++ Homework3/src/Animal/Cats.java
@@ -0,0 +1,15 @@
+package Animal;
+
+public class Cats extends Animals {
+    private static int count;
+
+    public Cats(int runMaxDistance, int swimMaxDistance) {
+        super(runMaxDistance, swimMaxDistance);
+        count++;
+
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
